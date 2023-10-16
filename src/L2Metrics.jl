f4a182cfc55d1b48173f59e78d0cd05fee82bc08
Sasha Petrenko <sap625@umsystem.edu>
2023-10-16 16:07:53 -0500

    Init package module

--- src/L2Metrics.jl
+++ src/L2Metrics.jl
@@ -1,5 +1,30 @@
+"""
+    L2Metrics.jl
+
+# Description
+TODO
+
+# Authors
+- Sasha Petrenko <petrenkos@mst.edu> @AP6YC
+"""
+
+"""
+A module that wraps the `l2logger` and `l2metrics` Python packages.
+
+# Imports
+
+The following names are imported by the package as dependencies:
+$(IMPORTS)
+
+# Exports
+
+The following names are exported and available when `using` the package:
+$(EXPORTS)
+"""
 module L2Metrics
 
-# Write your package code here.
+using
+    DocStringExtensions,
+    JSON
 
 end
